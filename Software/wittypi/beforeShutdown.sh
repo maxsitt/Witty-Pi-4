a06ec698cde7a18bc311f3b2126272066ae08ee3
Sittinger <87697856+maxsitt@users.noreply.github.com>
2025-02-14 12:15:21 +0100

    Update beforeShutdown.sh
    Send SIGTERM signal to running python3 processes if shutdown signal is received (e.g. from button press). Try to terminate processes gracefully with timeout (Witty Pi cuts power after 25s).

--- Software/wittypi/beforeShutdown.sh
+++ Software/wittypi/beforeShutdown.sh
@@ -8,3 +8,24 @@
 # Remarks: please use absolute path of the command, or it can not be found (by root user).
 # Remarks: you may append '&' at the end of command to avoid blocking the main daemon.sh.
 #
+# Includes modifications from https://github.com/maxsitt/Witty-Pi-4
+
+MAX_WAIT=15
+COUNTER=0
+
+if /usr/bin/pgrep python3 >/dev/null; then
+    # Send SIGTERM signal to all running python3 processes (graceful termination)
+    /usr/bin/killall -15 python3
+
+    # Wait for processes to terminate with timeout
+    while /usr/bin/pgrep python3 >/dev/null; do
+        /usr/bin/sleep 1
+        ((COUNTER++))
+
+        # Send SIGKILL signal if timeout reached (force termination)
+        if [ $COUNTER -ge $MAX_WAIT ]; then
+            /usr/bin/killall -9 python3
+            break
+        fi
+    done
+fi
